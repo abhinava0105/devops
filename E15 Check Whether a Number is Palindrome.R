cbee6fb7f8b64ee10cad06dcbb7bb944a8605a9f
abhinava0105 <abhinava0105@gmail.com>
2026-09-02 07:16:27 +0530

    Add R script to check if a number is a palindrome

---
+++ E15 Check Whether a Number is Palindrome.R
@@ -0,0 +1,15 @@
+n <- as.integer(readline("Enter a number: "))
+
+temp <- n
+rev <- 0
+
+while (temp > 0) {
+  rem <- temp %% 10
+  rev <- rev * 10 + rem
+  temp <- temp %/% 10
+}
+
+if (n == rev)
+  print("Palindrome")
+else
+  print("Not a Palindrome")
